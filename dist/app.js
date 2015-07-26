823cd74b7dee346290740275efb4b86be42516d9
Craig Michael Thompson <craig@craigsworks.com>
2015-07-26 22:59:02 +0100

    Compile files

--- dist/app.js
+++ dist/app.js
@@ -85,7 +85,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/durandal-webpack/";
+/******/ 	__webpack_require__.p = "/durandal-webpack/dist/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
