7eeb01352a8a8319afb23ae2ca442f3a95b6765b
Diana Sacuiu <pixelized_peanut@icloud.com>
2015-11-17 17:47:34 +0000

    fixed some tags and links

--- dist/app.js
+++ dist/app.js
@@ -85,7 +85,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/durandal-webpack/dist/";
+/******/ 	__webpack_require__.p = "./dist/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
